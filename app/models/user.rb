bb8218f70e2a33bb291a329a9a5edfa11cd7256f
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-15 20:21:27 +0900

    新規登録/本人情報確認　
    大文字　カタカナの指定
    pass の半角英数字混合指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          validates :nickname,:first_name, :last_name, :first_name_kana, :last_name_kana,:birthday, 
          presence: true
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+         validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/ }
+         validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/ }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
