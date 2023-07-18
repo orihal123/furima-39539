eabf9650f70a765a98ec7d5fb3756a3efcbbab3f
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-18 17:30:58 +0900

    バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,8 @@ class ItemsController < ApplicationController
 
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
-
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
 
   has_one_attached :image
+  belongs_to :category
+
+  belongs_to :user
+  
+  validates :item_name, :content, :category_id, :condition_id, :shipping_fee_burden_id, 
+            :prefecture_id, :shipping_duration_id, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+      
   
 end
