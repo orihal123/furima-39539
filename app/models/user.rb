9a7a3ce951cf711c6ec24b115b0c194fac2ffb31
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-18 16:57:07 +0900

    Active Storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :password, format: { with: VALID_PASSWORD_REGEX }
+        
 
          validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
          validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/ }
