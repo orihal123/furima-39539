149c297096c8315ea416bbb11514221e3c6997da
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-23 14:12:49 +0900

    商品出品機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(article_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -21,7 +21,13 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.
+    require(:item)
+    .permit(:item_name,:category_id, :condition_id, :content, :image, 
+            :shipping_fee_burden_id, :prefecture_id, :shipping_duration_id, :price )
+    .merge(user_id: current_user.id)
   end
   
 end
+
+#コメントをするときは＃でコメントアウトするようにしようね
