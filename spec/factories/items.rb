c45d6c5db2b249ff0d41f943c1c47a7b1d042fba
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-26 21:56:16 +0900

    修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,10 +1,40 @@
 FactoryBot.define do
   factory :item do
-    item_name                {'test'}
-    content                  {'test@example'}
+    item_name                { 'test' }
+    content                  { 'test@example' }
     price                    { rand(300..9999999) }
+    category_id              { 2 }   # 適切なカテゴリーのIDを指定
+    condition_id             { 2 }  # 適切な商品の状態のIDを指定
+    shipping_fee_burden_id   { 2 }  # 適切な配送料の負担のIDを指定
+    prefecture_id            { 2 }  # 適切な発送元の地域のIDを指定
+    shipping_duration_id     { 2 }  # 適切な発送までの日数のIDを指定
     
     association :user
-    
   end
 end
+
+
+
+
+
+# 正常系
+
+# ・全ての項目が入力されていれば出品できる
+
+# 異常系
+
+# ■商品画像
+# ・商品画像が空では出品出来ない（@item.imageには空文字ではなくnilを代入する）
+
+# ・
+# ・
+# ・
+# ・
+# ・
+# ■価格 
+# ・
+# ・価格に半角数字以外が含まれている場合は出品できない（※半角数字以外が一文字でも含まれていれば良い）
+# ・
+# ・
+# ■出品者情報
+# ・userが紐付いていなければ出品できない
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,54 +2,76 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
+
     @item = FactoryBot.build(:item)
+
+     # 画像のテスト用のファイルパスを指定
+     test_image_path = Rails.root.join('spec', 'fixtures', 'test_image.jpg')
+     test_image = Rack::Test::UploadedFile.new(test_image_path, 'image/jpeg')
+     @item.image = test_image # テスト用の画像を直接代入
+
+    
   end
 
   describe '必要な情報を適切に入力して「出品する」ボタンを押すと、商品情報がデータベースに保存されること。' do
+    context '全ての項目が入力されていれば出品できる'
+      
+      it '全ての項目が入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+
+      
+
     context '商品を出品できない場合' do
       
-      it '商品画像を1枚つけることが必須であること。' do  
-        expect(@item).not_to be_valid
-        expect(@item.errors[:image]).to include("can't be blank")
+      it '商品画像が空では出品出来ない' do 
+        @item.image = nil 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end     
-      
-      it '商品名が空では登録できない' do
+
+      it '商品名が空では出品できない' do
         @item.item_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include ("Item name can't be blank")
       end
+      
+      it '商品説明が空では出品できない' do
+        @item.content = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include ("Content can't be blank")
+      end
 
-      it 'カテゴリーの情報が空では登録できない' do
-        @item.category_id= ''
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id= nil
         @item.valid?
         expect(@item.errors.full_messages).to include ("Category can't be blank")
       end
-
-      it '商品の状態の情報が空では登録できない' do
-        @item.condition_id= ''
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.condition_id= nil
         @item.valid?
         expect(@item.errors.full_messages).to include ("Condition can't be blank")
       end
 
-      it '配送料の負担の情報が空では登録できない' do
-        @item.shipping_fee_burden_id = ''
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.shipping_fee_burden_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include ("Shipping fee burden can't be blank")
       end
 
-      it '発送元の地域の情報が空では登録できない' do
-        @item.prefecture_id = ''
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include ("Prefecture can't be blank")
       end
 
-      it '発送までの日数の情報が空では登録できない' do
-        @item.shipping_duration_id  = ''
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.shipping_duration_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include ("Shipping duration can't be blank")
       end
       
-      it '価格の情報が空では登録できない' do
+      it '価格が空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include ("Price can't be blank")
@@ -58,27 +80,20 @@ RSpec.describe Item, type: :model do
 
     context '価格は、¥300~¥9,999,999の間のみ保存可能であること。' do
       
-      it '価格は、¥300以下では登録できない' do
+      it '価格が300円未満では出品できない' do
         @item.price = "100"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
-      it '価格は9,999,999以上では登録できない' do
+      it '価格が9_999_999円を超えると出品できない' do
         @item.price = "10000000"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-    end
 
-    context '価格は半角数値以外は登録できない。' do
-      it '半角数値の場合は登録できること' do
-        @item.price = "500"
-        @item.valid?
-      end
-
-      it '半角数値以外の場合は登録できないこと' do
-        @item.price = '１２３４'
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = '1２345'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end  
