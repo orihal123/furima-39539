2ee7668c67e84d1ba239618325c6ec5965a9e63b
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-26 15:45:37 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,17 +2,32 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :category
-
   belongs_to :user
   
   validates :item_name, :content, :category_id, :condition_id, :shipping_fee_burden_id, 
             :prefecture_id, :shipping_duration_id, presence: true
-  validates :price, presence: true, numericality: { only_integer: true }
+  validates :price, presence: true, numericality: {
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999,
+              only_integer: true
+            }, format: { with: /\A\d+\z/, message: "Only single-byte alphanumeric characters are allowed" }
+
+  validate :validate_image_presence
+
+  
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_burden_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
+
+  def validate_image_presence
+    unless image.attached?
+      errors.add(:image, "can't be blank")
+    end
+  end
       
   extend ActiveHash::Associations::ActiveRecordExtensions
 
