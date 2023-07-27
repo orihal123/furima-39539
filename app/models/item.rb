607f5f38e1aa099bb6b6677e85a5fbcc7aad922c
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-27 20:07:27 +0900

    修正版

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
               greater_than_or_equal_to: 300,
               less_than_or_equal_to: 9_999_999,
               only_integer: true
-            }, format: { with: /\A\d+\z/, message: "Only single-byte alphanumeric characters are allowed" }
+            }
 
   validate :validate_image_presence
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,15 +13,15 @@ RSpec.describe Item, type: :model do
     
   end
 
-  describe '必要な情報を適切に入力して「出品する」ボタンを押すと、商品情報がデータベースに保存されること。' do
+  describe '商品出品機能' do
     context '全ての項目が入力されていれば出品できる'
       
       it '全ての項目が入力されていれば出品できる' do
         expect(@item).to be_valid
       end
+    end
 
-      
-
+    
     context '商品を出品できない場合' do
       
       it '商品画像が空では出品出来ない' do 
@@ -76,9 +76,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include ("Price can't be blank")
       end
-    end
-
-    context '価格は、¥300~¥9,999,999の間のみ保存可能であること。' do
+    
       
       it '価格が300円未満では出品できない' do
         @item.price = "100"
@@ -98,7 +96,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end  
 
-    end
+    
       it 'ユーザーが紐付いていなければ投稿できない' do
         @item.user = nil
         @item.valid?
