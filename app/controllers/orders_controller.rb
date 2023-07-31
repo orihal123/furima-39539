51999aa908a7d7452c6c8ada4984ef5d62d7f30e
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-07-31 21:29:23 +0900

    フォームオブジェクトを使ってのテーブル保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,27 @@
 class OrdersController < ApplicationController
-
-
+  
+  
   def index
-    # @item = Item.find(params[:item_id])
-   
-    # indexアクションの処理を記述
-  end
-
-  def new
     @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.create(order_params)
-    Address.create(address_params)
-    redirect_to root_path
+
+    
+    @order_address = OrderAddress.new(order_params)
+
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
-
   def order_params
-  params.require(:order).permit(:user_id, :item_id, :postal_code, :prefecture_id, :municipalities,
-                                :address_information, :building_name, :telephone_number)
-        .merge(user_id: current_user.id)
-end
-
-
+    params.require(:order_address)
+          .permit(:postal_code, :prefecture_id, :municipalities, :address_information, :building_name, :telephone_number)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -52,11 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_053616) do
     t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
-  create_table "categories", charset: "utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "content", null: false
@@ -73,8 +68,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_053616) do
   end
 
   create_table "orders", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -99,4 +98,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_053616) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
