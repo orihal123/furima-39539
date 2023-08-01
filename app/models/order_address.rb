1bbb260f8f8462dfb20942f09e2c03f9c9c997c3
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-01 17:03:10 +0900

    テストコード完成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,11 +3,12 @@ class OrderAddress
   attr_accessor :postal_code, :prefecture_id, :municipalities, :address_information, :building_name,
                 :telephone_number,:user_id, :item_id, :token
 
-  validates :prefecture_id, :municipalities, :address_information, :building_name,
+  validates :municipalities, :address_information, 
             :user_id, :item_id, presence: true
   validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "please enter in the format of 3 digits, hyphens and 4 digits" }
   validates :telephone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: "please enter a half-width number between 10 and 11 digits" }
   validates :token, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
 
   def save
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_address do
     postal_code             { '123-4567' }
-    prefecture_id           { 1 }
+    prefecture_id           { 2 }
     municipalities          { '板橋区' }
     address_information     { '1-1' }
     building_name           { '折原ハイツ' }
