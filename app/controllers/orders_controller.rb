1ee0cdb2d8e646767ce4719b5f78d7586691e463
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-01 14:46:48 +0900

    クレジット決済機能の実装完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
@@ -26,7 +27,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-  Payjp.api_key = "sk_test_91be645d1f9911589539662e"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
       Payjp::Charge.create(
         amount: @item.price,  # 商品の値段
         card: order_params[:token],    # カードトークン
