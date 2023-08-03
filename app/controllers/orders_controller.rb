2ef0c66ae21de38441ad63868868f68e7c7bb843
orihal123 <138180298+orihal123@users.noreply.github.com>
2023-08-03 15:05:01 +0900

    挙動の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
- 
+  before_action :set_gon, only: [:index, :create]
 
   def index
     
-    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+   
     @order_address = OrderAddress.new
 
     return unless user_signed_in?
@@ -34,6 +34,10 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def set_gon
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def order_params
